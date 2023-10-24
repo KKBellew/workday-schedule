b2f9cc5fbc306346e6d67ff365c5d25f3f55f48f
kkbellew <kkbellew@gmail.com>
2023-10-23 21:47:32 -0500

    finishing touches on js and html

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var today = dayjs().format('dddd, MMMM DD');
-
 var now = dayjs().format("H A");
-
-var workHours = [
+var currentHr= new Date().getHours()
+$("#currentDay").text(today);
+currentHour =  12;
+    
+var workHours = [ 
   {time: "8AM", event:"" },
   {time: "9AM", event:"" },
   {time: "10AM", event:"" },
@@ -14,47 +14,73 @@ var workHours = [
   {time: "2PM", event:"" },
   {time: "3PM", event:"" },
   {time: "4PM", event:"" },
-  {time: "5PM", event:"" },
-  {time: "6PM", event:"" },
-];
+  ];
+
+function GetArrayIndexForTime(timeStr) {
+	let retIndex = -1;
+	workHours.forEach((value,index)=>{
+		if (value.time.toLowerCase().trim() === timeStr.toLowerCase().trim()) retIndex = index;
+	});
+	return retIndex;
+}
+function GetHrStr(id){
+  var splitHrDispID = id.split("-");
+  var hr = parseInt(splitHrDispID[1]);
+	var hrStr = "";
+	if (hr > 12) hrStr = (hr % 12) + "PM"; 
+	else hrStr = hr + "AM";
+	return hrStr;
+}
 
 $(function () {
-
-//consol.log(dayjs().format("dddd, MMMM DD"))
-$("#currentDay").text(today);
-
-  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+	$(".description").val("");
+	$("#currentDay").text(today);
+	
+	var workEvents = JSON.parse(localStorage.getItem("workHours"));
+	if (workEvents){
+	  workHours = workEvents;
+	  console.log(workHours);
+	  
+	  workHours.forEach((x)=>{
+		  var hr = parseInt(x.time.toLowerCase().trim().replace("am","").replace("pm",""));
+		  if(x.time.toLowerCase().trim().includes("pm") && hr != 12) hr = hr + 12;
+		  $('#hour-'+ hr).children(".description").val(x.event);
+	  })
+	}
+$(".saveBtn").on("click",function(){
+  var hrStr = GetHrStr($(this).parent().attr("id"));
+	var workHoursArrIndex = GetArrayIndexForTime(hrStr);
+		if(workHoursArrIndex > 0){
+			workHours[workHoursArrIndex].event = $(this).parent().children(".description").val();
+			console.log(workHours);
+			localStorage.setItem("workHours", JSON.stringify(workHours));
+    }
+  });
+	
+$("[id^=hour-]").each((index, value)=>{
+	var itemHrStr = $(value).children(".hour").html();
+	console.log(value);
+	console.log(itemHrStr);
+	var hr = parseInt(itemHrStr.replace("pm").replace("am"));
+	if(itemHrStr.toLowerCase().trim().includes("pm") && hr != 12) hr = hr + 12
+  console.log(hr);
+	if(hr > currentHr) $(value).addClass("future");
+	else if(hr < currentHr) $(value).addClass("past");
+  else $(value).addClass("present");
+	});
 });
 
-//local storage check
-var workEvents = JSON.parse(localStorage.getItem("workHours"));
-if (workEvents){
-  workHours = workEvents;
-}
-//color rows and function
 function colorRow(time) {
 	var eventNow = format(now, "H A");
 	var entryTime = format(time, "H A");
 	if (eventNow.isBefore(entryTime) === true) {
-		return "future";
+    return "future";
 	} else if (eventNow.isAfter(entryTime) === true) {
 		return "past";
 	} else {
-		return "present";
+		return "current";
 	}
-}
+};
 
-//save btn function
+
+
